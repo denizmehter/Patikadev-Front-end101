db69fb4296628c4972ce3ad8cd7fc86b6a3477ae
Deniz Mehter <denizmehter@gmail.com>
2023-01-20 10:17:33 +0300

    HW2 Finished

--- Javascript/Calisma/script.js
+++ Javascript/Calisma/script.js
@@ -1,30 +1,42 @@
 let formDOM = document.querySelector('#userForm')
 formDOM.addEventListener('submit', eventHandler)
 let alarm = document.querySelector('#alert');
-const ALERT = (title, message, sinif=warning) => `<div class="alert alert-${sinif} alert-dismissible fade show" role="alert">
-<strong>${title}</strong> ${message}
-<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>`
+const ALERT = (message, sinif) => `<span class="alert alert-${sinif} alert-dismissible fade show" role="alert">${message}
+</span>`
+
+let deleteDOM = document.querySelector('#delete');
+deleteDOM.addEventListener('click', clickeventHandler);
+
+function clickeventHandler(){
+    let listDOM = document.querySelector('#user_list');
+    let sayı = listDOM.childElementCount;
+    let checkDOM = document.querySelectorAll('.form-check-input')
+    for (let i = 0; i < sayı; i++) {
+        if (checkDOM[i].checked) {
+            listDOM.removeChild(listDOM.children[i]);
+        }
+    }
+    alarm.innerHTML = ALERT("Not Silindi", "danger");
+}
 
 function eventHandler(event){
     event.preventDefault();
     let user_name = document.querySelector('#full_name');
-    let user_mail = document.querySelector('#email');
-    if (user_mail.value && user_name.value) {
-        addItem(user_name,user_mail);
+    if (user_name.value) {
+        addItem(user_name);
         user_name.value = "";
-        user_mail.value = "";
-        alarm.innerHTML = ALERT("Başarılı!","Bilgiler Kaydedildi","success");
+        alarm.innerHTML = ALERT("Not Eklendi","success");
     }
     else {
-        alarm.innerHTML = ALERT("Eksik Bilgi","Eksik Alanları Doldurunuz!","danger");
+        alarm.innerHTML = ALERT("Not Alanını Doldurun", "warning");
     }
 }
 
-let addItem = (isim,mail) => {
+let addItem = (isim) => {
     const list = document.querySelector('#user_list');
     let liDOM = document.createElement('li');
-    liDOM.innerHTML = `${isim.value} - ${mail.value}`
-    liDOM.classList.add(`list-group-item`)
+    liDOM.innerHTML = `<input class="form-check-input me-3" type="checkbox" value="">${isim.value}`;
+    liDOM.classList.add("list-group-item");
     list.appendChild(liDOM);
 }
+
